9fa424c9ad7306e5a6c6ec44e499f306a1d7bdb8
macbook <macbook@Master2011v1.0>
2018-10-31 10:43:43 +0100

    Updates

---
+++ precache-manifest.4bc1ae81eb8ba65ddab949492464e0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "456939b92d1a95a069a3",
+    "url": "/contactmanager/static/js/main.456939b9.chunk.js"
+  },
+  {
+    "revision": "680a9ba103bf9b524dd9",
+    "url": "/contactmanager/static/js/1.680a9ba1.chunk.js"
+  },
+  {
+    "revision": "456939b92d1a95a069a3",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "680a9ba103bf9b524dd9",
+    "url": "/contactmanager/static/css/1.181b422d.chunk.css"
+  },
+  {
+    "revision": "3855ad13b9060ef95839f0140792d3c9",
+    "url": "/contactmanager/index.html"
+  }
+];
